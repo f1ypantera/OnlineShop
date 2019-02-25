e320ea2bb50d682bf1eb3087cb76a37dec03cbca
Vadym Tselikin <Vadym.Tselikin@am.jll.com>
2019-02-25 16:45:05 +0200

    Cartline

---
+++ OnlineShop/Models/Cart.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OnlineShop.Models;
+
+namespace OnlineShop.Models
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(CarModel carModel)
+        {
+            CartLine line = lineCollection
+                .Where(g => g.CarModel.CarModelId == carModel.CarModelId)
+                .FirstOrDefault();
+            if(line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    CarModel = carModel
+                });
+            }
+
+        }
+
+        public void RemoveLine(CarModel carModel)
+        {
+            lineCollection.RemoveAll(l => l.CarModel.CarModelId == carModel.CarModelId);
+        }
+        public decimal ComputeTotalValue()
+        {
+            return lineCollection.Sum(e => e.CarModel.Price);
+        }
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+        public IEnumerable<CartLine> Lines
+        {
+            get { return lineCollection; }
+        }
+
+    }
+
+    public class CartLine
+    {
+        public CarModel CarModel { get; set; }
+    }
+}
