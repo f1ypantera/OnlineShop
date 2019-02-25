6430a6a502c8739f5eb14bc80cb5f58f42f8c3e4
Vadym Tselikin <Vadym.Tselikin@am.jll.com>
2019-02-25 14:41:23 +0200

    Navigation panel created

---
+++ OnlineShop/Controllers/NavigationController.cs
@@ -0,0 +1,35 @@
+﻿using OnlineShop.Interface;
+using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Controllers
+{
+    public class NavigationController : Controller
+    {
+        private readonly IRepository<CarModel> carRepository;
+
+
+        public NavigationController(IRepository<CarModel> carRepository)
+        {
+            this.carRepository = carRepository;
+
+        }
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = carRepository.GetAll()
+                .Select(m => m.NameCategory)
+                .Distinct()
+                .OrderBy(x => x);
+            return PartialView(categories);
+        }
+
+
+      
+     
+       
+    }
+}
