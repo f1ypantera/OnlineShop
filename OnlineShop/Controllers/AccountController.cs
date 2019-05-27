0304d56a00ac2a7897a3e06b0f9136d1a2a61a88
f1ypantera <flyypaantera@gmail.com>
2019-05-27 16:17:39 +0300

    Update before create imgae

--- OnlineShop/Controllers/AccountController.cs
+++ OnlineShop/Controllers/AccountController.cs
@@ -1,17 +1,48 @@
-﻿using System;
+﻿using OnlineShop.Interface;
+using OnlineShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace OnlineShop.Controllers
 {
     public class AccountController : Controller
     {
         // GET: Account
-        public ActionResult Index()
+        IAuthProvider authProvider;
+        public AccountController(IAuthProvider auth)
+        {
+            authProvider = auth;
+        }
+
+        public ViewResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Login model, string returnUrl)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (authProvider.Authenticate(model.UserName, model.Password))
+                {
+                    return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неправильный логин или пароль");
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
--- OnlineShop/Models/CarModel.cs
+++ OnlineShop/Models/CarModel.cs
@@ -25,5 +25,7 @@ namespace OnlineShop.Models
         [Required(ErrorMessage = "Введите цену")]
         [Range(0.01, double.MaxValue, ErrorMessage = "Пожалуйста, введите положительное значение для цены")]
         public decimal Price { get; set; }
+        public byte[] ImageData { get; set; }
+        public string ImageMimeType { get; set; }
     }
 }
