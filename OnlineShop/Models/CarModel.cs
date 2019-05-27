1bfa6cb57f54c2bfc21ee9a6cbd7b7f04af48696
f1ypantera <flyypaantera@gmail.com>
2019-05-27 15:14:39 +0300

    Update AdminPanel

--- OnlineShop/Models/CarModel.cs
+++ OnlineShop/Models/CarModel.cs
@@ -10,14 +10,20 @@ namespace OnlineShop.Models
     {
         public int CarModelId { get; set; }
         [Display(Name = "Модель")]
+        [Required(ErrorMessage ="Введите модель")]
         public string NameModel { get; set; }
         [Display(Name = "Производитель")]
+        [Required(ErrorMessage = "Введите производителя")]
         public string NameManufacturer { get; set; }
         [Display(Name = "Категория")]
+        [Required(ErrorMessage = "Введите категорию")]
         public string NameCategory { get; set; }
         [Display(Name = "Описание")]
+        [Required(ErrorMessage = "Введите описание")]
         public string Description { get; set; }
-        [Display(Name = "Цена")]
+        [Display(Name = "Цена $$$")]
+        [Required(ErrorMessage = "Введите цену")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Пожалуйста, введите положительное значение для цены")]
         public decimal Price { get; set; }
     }
 }
