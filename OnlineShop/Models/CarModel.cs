19aa98326898edbe12d734d315ebaafc814693a0
f1ypantera <flyypaantera@gmail.com>
2019-05-27 14:05:33 +0300

    Update CarModel

--- OnlineShop/Models/CarModel.cs
+++ OnlineShop/Models/CarModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,15 @@ namespace OnlineShop.Models
     public class CarModel
     {
         public int CarModelId { get; set; }
+        [Display(Name = "Модель")]
         public string NameModel { get; set; }
+        [Display(Name = "Производитель")]
         public string NameManufacturer { get; set; }
+        [Display(Name = "Категория")]
         public string NameCategory { get; set; }
+        [Display(Name = "Описание")]
         public string Description { get; set; }
+        [Display(Name = "Цена")]
         public decimal Price { get; set; }
     }
 }
